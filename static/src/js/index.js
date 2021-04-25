ea595ef929b248ad105cbc05d457a322f2834284
YeffersonEstrada <yeffersone@gmail.com>
2021-04-25 18:22:57 -0500

    agregando requerimientos pero aun no lo logro :(

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -4,6 +4,9 @@ const api = new API()
 let currentCaracter = 1;
 const $loadNext = document.querySelector('#load-next')
 
+
+
+
 $loadNext.addEventListener('click', async () => {
   const characterData = await api.getCharacter(++currentCaracter)
   new Character(characterData)
@@ -12,14 +15,34 @@ $loadNext.addEventListener('click', async () => {
 async function initApp(initCharacterId) {
   const characterData = await api.getCharacter(initCharacterId)
   console.log(characterData)
-  new Character(characterData)
-}
+
+  if(characterData.status === 'Alive'){
+    const characterData = await api.getCharacter(initCharacterId)
+    console.log(characterData.status)
+    new Character(characterData)
+    return Character
+  } // if (characterData.status === 'unknown'){
+//     console.log(characterData)
+//     alert('Este personaje tiene un status Unknown')
+//     console.log(api.characterData.status)
+//     return characterData
+
+//   // new Character(characterData)
+// }else if(characterData.status === 'Dead')
+// console.log(characterData)
+// alert('Este personaje tiene un status Unknown')
+// console.log(api.characterData.status)
+// return characterData
+
+  }
+
+
 
 initApp(currentCaracter)
 
 // console.log(initApp(currentCaracter)
 // )
 
-//console.log(api.getCharacter(1))
+// console.log(api.getCharacter(20))
 
 
