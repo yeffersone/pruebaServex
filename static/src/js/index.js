fcde7ad826ebcb5501302cf195d124f4474391e9
YeffersonEstrada <yeffersone@gmail.com>
2021-04-24 21:10:01 -0500

    escala de grises em la imagen

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -1,1 +1,23 @@
-console.log("hola")
+import API from './api.js'
+import Character from './character.js'
+const api = new API()
+let currentCaracter = 1;
+const $loadNext = document.querySelector('#load-next')
+
+$loadNext.addEventListener('click', async () => {
+  const characterData = await api.getCharacter(++currentCaracter)
+  new Character(characterData)
+})
+
+async function initApp(initCharacterId) {
+  const characterData = await api.getCharacter(initCharacterId)
+  console.log(characterData)
+  new Character(characterData)
+}
+
+initApp(currentCaracter)
+
+
+//console.log(api.getCharacter(1))
+
+
