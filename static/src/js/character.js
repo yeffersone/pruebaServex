6c36d31d8660adfddfc2040b6eb41ec99a39de3c
YeffersonEstrada <yeffersone@gmail.com>
2021-04-24 22:55:25 -0500

    cambio en el constructor.

--- static/src/js/character.js
+++ static/src/js/character.js
@@ -2,7 +2,6 @@ export default class Character {
     constructor({ name, image, /*gender,*/ species, status }) {
       this.name = name
       this.image = image
-      // this.gender = gender
       this.species = species
       this.status = status
       this.$characterImageContainer = document.querySelector('#character-image-container')
